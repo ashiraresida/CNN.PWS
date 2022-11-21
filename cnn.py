2469e27fc535c81a0939cd967e5c919ee15014af
ashiraresida <112160497+ashiraresida@users.noreply.github.com>
2022-11-21 22:21:20 +0100

    Commit Changes

--- cnn.py
+++ cnn.py
@@ -75,6 +75,6 @@ test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
 training_set.class_indices
 if result[0][0] == 1:
-prediction = 'dog'
+    prediction = 'dog'
 else:
-prediction = 'cat'
+    prediction = 'cat'
